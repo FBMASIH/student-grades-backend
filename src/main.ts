3f4bbb770025d80ed3b5718f2b09327bb527a9bc
Farhad <Bigonahibit@gmail.com>
2025-07-28 20:58:22 +0330

    feat: refactor CORS configuration to enable specific settings for the application

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,19 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const jwtSecret = configService.get<string>('jwtSecret');
+
+  app.enableCors({
+    origin: 'http://185.105.184.157:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    exposedHeaders: ['Authorization'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('سیستم مدیریت نمرات')
