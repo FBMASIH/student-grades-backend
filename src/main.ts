261fd786c8be8535532ddf03e6786e2f247db5ca
Farhad <Bigonahibit@gmail.com>
2025-07-28 20:54:59 +0330

    feat: enable CORS support for the application

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
   const jwtSecret = configService.get<string>('jwtSecret');
-
-  app.enableCors({
-    origin: 'http://185.105.184.157:3000', // Replace with your front-end domain
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
 
   const config = new DocumentBuilder()
     .setTitle('سیستم مدیریت نمرات')
