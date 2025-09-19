9371c8cdccf739428f9fd44c6d70ac495a5daf5c
Farhad Bigonahi <44204832+FBMASIH@users.noreply.github.com>
2025-09-19 10:42:46 +0330

    Enhance group student lookup with course group support

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CourseAssignment } from '../course-assignments/entities/course-assignment.entity';
+import { CourseGroup } from '../course-groups/entities/course-group.entity';
 import { Enrollment } from '../enrollment/entities/enrollment.entity';
 import { Group } from './entities/group.entity';
 import { GroupsController } from './groups.controller';
 import { GroupsService } from './groups.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Group, Enrollment, CourseAssignment])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Group,
+      Enrollment,
+      CourseAssignment,
+      CourseGroup,
+    ]),
+  ],
   controllers: [GroupsController],
   providers: [GroupsService],
   exports: [GroupsService],
